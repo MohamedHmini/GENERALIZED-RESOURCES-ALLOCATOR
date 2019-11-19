db31d0edddafc459f5dc1309a0d5f4c39ed6c981
MohamedHmini <takarage1997@gmail.com>
2019-11-19 15:41:25 +0100

    ADDING EXECUTION TIME COUNTER

--- GRA_DP.c
+++ GRA_DP.c
@@ -1,9 +1,11 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
+#include<time.h>
 #include"HLCio/dataframe.h"
 #include"HLCio/advio.h"
 #include "GRA.h"
+
 
 
 void main(int argc, char *args[]){
@@ -14,6 +16,13 @@ void main(int argc, char *args[]){
     // display_df(r);
 	df_retype(r, DF_ELEMENT_TInt);
 
+    int i;
+	double total_time;
+	clock_t start, end;
+	start = clock();
+	//time count starts 
+	srand(time(NULL));
+    
     DATAFRAME *f = fit_to_find_multiple_optimal_paths(r);
     // display_df(f);
     GRA_RESULTS *results = gracreate();
@@ -21,8 +30,15 @@ void main(int argc, char *args[]){
     results->policies = arrcreate(0);
     DF_ELEMENT policy = arrcreate(0);
     load_policies(f, f->len_rows, f->len_cols - 1, &policy, &results->policies);
+    
+	end = clock();
+	//time count stops 
+	total_time = ((double) (end - start)) / CLK_TCK;
+	//calulate total time
+    
     display_results(*results);
 
+	printf("\nTHE EXECUTION TIME REQUIRED FOR THAT ALGORITHM IS : %f s", total_time);
     arrfree(&results->policies);
     arrfree(&policy);
     df_free(f);
